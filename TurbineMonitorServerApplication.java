2262072c2f7ccae6bad19e785c43d8fac8267858
xiaoyunz <416912931@qq.com>
2018-08-19 11:48:50 +0800

    223

--- TurbineMonitorServerApplication.java
+++ TurbineMonitorServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * 断路器聚合监控(Hystrix Turbine)
  * 查看turbine.stream
  * http://192.168.14.127:8766/turbine.stream
- * 查看Hystrix Turbine图形展示 http://192.168.14.127:8766/hystrix
+ * 查看Hystrix Turbine图形展示 http://192.168.14.127:8766/hystrix 
  * 在界面依次输入：http://192.168.14.127:8766/turbine.stream 、2000 、zhouxy 
  */
 @SpringBootApplication
